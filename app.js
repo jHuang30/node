f89c1da097c4db541524195f9a6f462505f2ea49
jhuang30 <jieyinghuang30@gmail.com>
2019-08-10 11:39:26 -0400

    ajax

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("hello there");
+// });
+
+// app.get("/example", (req, res) => {
+//   console.log(req.query);
+//   res.send(req.query);
+// });
+
+app.use("/public", express.static(path.join(__dirname, "static")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ success: true });
+});
+app.listen(3000);
---
+++ mod.js
@@ -0,0 +1,3 @@
+const _ = require("lodash");
+let example = _.fill([1, 1, 2, 2, 3], "apple", 1, 5);
+console.log(example);
